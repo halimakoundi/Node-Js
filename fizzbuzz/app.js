e97acd29f6b6784940b62613dde108aaa07b9236
Halima KOUNDI <hkoundi@gmail.com>
2016-11-23 14:54:02 +0000

    fizzbuzz web api with express

---
+++ fizzbuzz/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fizzbuzz = require('./fizzbuzz');
+var express = require('express');
+var app = express();
+
+app.use(authenticate);
+app.get('/fizz(buzz)/:number', function (req, res, next) {
+    var generatedResult ;
+
+        generatedResult = fizzbuzz.generate(req.params.number) ;
+        res.send('FizzBuzz generated: ' + "<br/>" + generatedResult + "<br/>" );
+
+});
+
+app.use(errorHandler);
+
+function errorHandler (err, req, res, next) {
+    res.status(500);
+    console.error(err.stack);
+    res.send('error');
+}
+
+function authenticate(req, res, next) {
+
+    if(req.query.userid !== 'bob'){
+        res.status(401);
+        res.send("Authentication failed. Ask for a user name!");
+        return;
+    }
+    next();
+}
+
+app.listen(3000, function () {
+    console.log('The FizzBuzz app is listening on port 3000!');
+});
---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function makeFizzBuzz(){
+    return new FizzBuzz();
+}
+class FizzBuzz {
+
+    generate(value) {
+
+        if(Number.isInteger(parseInt(value))){
+
+            if (isFizzBuzz(value)) {
+                return "fizz buzz";
+            }
+            if (isFizz(value)) {
+                return "fizz";
+            }
+            if(isBuzz(value)) {
+                return "buzz";
+            }
+            return value;
+        }else{
+            throw Error("Please provide a number");
+        }
+    }
+}
+
+function isFizz(value) {
+    return (value % 3 == 0);
+}
+
+function isBuzz(value) {
+    return  value % 5 == 0;
+}
+
+function isFizzBuzz(value) {
+    return isFizz(value)  && isBuzz(value);
+}
+
+module.exports = {generate : makeFizzBuzz().generate};
